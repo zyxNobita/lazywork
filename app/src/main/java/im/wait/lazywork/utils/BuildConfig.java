e49287f42a4cec10d7e9e27341a43e774a050f4b
thinkloki <thinkloki@gmail.com>
2015-08-07 23:48:08 +0800

    第一次提交

---
+++ app/src/main/java/im/wait/lazywork/utils/BuildConfig.java
@@ -0,0 +1,10 @@
+package im.wait.lazywork.utils;
+
+/**
+ * Created by thinkloki on 2015/8/7.
+ * Email: thinkloki@gmail.com
+ * joyworks
+ */
+public class BuildConfig {
+    public static boolean DEBUG = true;
+}
